6e2db9e3097114e7151fe63a4f009c413f22a08f
tbryan <wcffgkobe@163.com>
2021-02-02 14:05:50 +0800

    11

---
+++ src/Alinpay/Alinpay.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Created by haihuike.
+ * @User: wangchengfei
+ * @Date: 2021/2/2
+ * @Time: 13:29
+ */
+
+namespace tbryan24\Alinpay;
+
+
+class Alinpay
+{
+    function test(){
+        echo 1111;
+    }
+}
